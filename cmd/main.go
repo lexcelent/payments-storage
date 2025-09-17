0389746ec01f63551a0da701843a680ad74fa20e
Aleksey <lexanorilsk@gmail.com>
2025-09-17 10:47:32 +0300

    init commit

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/lexcelent/payments-storage/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	// init logger
+	log := setupLogger(cfg.Env)
+	log.Info("setup logger")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env        string     `yaml:"env"`
+	HTTPServer HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address string `yaml:"address"`
+	Port    string `yaml:"port"`
+}
+
+func MustLoad() *Config {
+	var path string
+
+	flag.StringVar(&path, "config", "", "path to config file")
+	flag.Parse()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config path does not exist: " + path)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
+	}
+
+	return &cfg
+}
